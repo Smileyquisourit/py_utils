ffd0ced8735c71954e50a840b6559678397ae756
Thibaut DERAINS <thibaut.derains@gmail.com>
2025-05-15 14:19:40 +0200

    Added RotaryFileHandler

--- Logueur/log_out.py
+++ Logueur/log_out.py
@@ -12,7 +12,10 @@ Module log_out
 Implement an abstract base class for defining an output for a logger. This abstract class implement how the messages
 are filtered, and define an abstract method that should define how a message is writted.
 
-This module also implement a log output to the console, and a log output to a file.
+This module also implement the following log's output:
+- ConsoleLogHandler
+- FileLogHandler
+- RotaryFileLogHandler
 
 Objects
 -------
@@ -26,6 +29,7 @@ FileLogHandler:
 
 #TODO: add a database output
 
+import re
 import os
 import sys
 import warnings
@@ -239,6 +243,7 @@ class ConsoleLogHandler(BaseLogHandler):
         out.write(msg_str)
         out.flush()
 
+
 class FileLogHandler(BaseLogHandler):
     """ 
     ==============
@@ -286,7 +291,7 @@ class FileLogHandler(BaseLogHandler):
 
         # Type Check:
         # -----------
-        if not filename and not isinstance(filename,str):
+        if filename and not isinstance(filename,str):
             raise ValueError(f"The filename must be a str, instead I've received a '{type(filename)}'")
         if not isinstance(action,str):
             raise ValueError(f"The action argument must be a str, instead I've received a '{type(action)}'")
@@ -353,3 +358,219 @@ class FileLogHandler(BaseLogHandler):
             filename = f"{base}_{i}{ext}"
             i += 1
         return filename
+
+
+
+
+class LogDirEntry(object):
+    """
+        A sort of subclass of the DirEntry returned by os.scandir(),
+        with a added attribut containing the time of creation determined
+        used the file's name.
+
+        Also contain a static method for reconstructing a time (an instance 
+        of datetime.datetime) determined using the file's name and a pattern.
+
+        All the methods/attributes of DirEntry are accesible by using the
+        __getattribute__ of the DirEntry in this class __getattr__.
+    """
+
+    def __init__(self,dirEntry, creationTime:datetime.datetime):
+
+        # Type check:
+        if not isinstance(creationTime,datetime.datetime):
+            raise TypeError(f"The creation time should be a 'datetime.datetime' object, instead I've received a {type(creationTime)}")
+        self._dirEntry = dirEntry
+        self.creationTime = creationTime
+
+    def __getattr__(self, name):
+        return self._dirEntry.__getattribute__(name)
+    
+    def __repr__(self):
+        return f"<LogDirEntry '{self.name}'>"
+    
+    def __str__(self):
+        return self.__repr__()
+
+class RotaryFileLogHandler(BaseLogHandler):
+
+    _ISO_FMT_STR_PATTERN = \
+        r"{base}(?P<date>\d{{4}}-\d{{2}}-\d{{2}}){timeSep}(?P<time>\d{{2}}:\d{{2}}:\d{{2}})" + \
+        r"(?>\.(?P<time_ms>\d{{6}}))?(?P<tz>[+-]\d{{2}}:\d{{2}})?{ext}"
+    
+    _LOG_FILE_FMT = "{base}{{date}}{timeSep}{{time}}{ext}"
+    
+    _SUPPORTED_LOG_UNIT = ('file','day','week','month')
+
+    @property
+    def logfiles(self) -> tuple[LogDirEntry]:
+        """ 
+            A tuple of LogDirEntry obtained with `os.scandir`.
+
+            An attribute `creationTime` is added wich represent the creation time
+            of the file using it's name.
+        """
+        _logFiles =  tuple(entry for entry in os.scandir(self._directory) if self._filename_pattern.fullmatch(entry.name))
+        _creationTimes = tuple(self._reconstruct_time_from_filename(file.name) for file in _logFiles)
+        return tuple(LogDirEntry(file,createdTime) for file, createdTime in zip(_logFiles,_creationTimes))
+
+
+    def __init__(self, level: LogLevel, filter: LogTopicFilter, baseFilename:str="log_", directory:str='.',
+                 log_ext: str = '', timeSep:str='T', tz:datetime.timezone=datetime.timezone.utc,
+                 maxSizeFile:int=10*1024**2, maxLog:int=5, maxLogUnit:str='file'):
+        
+        # Type/Value Check:
+        # -----------------
+        if not isinstance(baseFilename,str):
+            raise TypeError(f"The filename must be a str, instead I've received a '{type(baseFilename)}'")
+        if not isinstance(log_ext,str):
+            raise TypeError(f"The filename extension (log_ext) must be a str, instead I've received a '{type(log_ext)}'")
+        if not isinstance(directory,str):
+            raise TypeError(f"The directory must be a str, instead I've received a '{type(directory)}'")
+        if not isinstance(timeSep,str):
+            raise TypeError(f"The time / date separator (timeSep) must be a str, instead I've received a '{type(timeSep)}'")
+        if not isinstance(tz,datetime.timezone):
+            raise TypeError(f"The timezone (tz) must be a 'datetime.timezone', instead I've received a '{type(tz)}'")
+        if not isinstance(maxSizeFile,int):
+            raise TypeError(f"The maximum size of file (maxSizeFile) must be a int, instead I've received a '{type(maxSizeFile)}'")
+        if not isinstance(maxLog,int):
+            raise TypeError(f"The maximum number of file (maxLog) must be a int, instead I've received a '{type(maxLog)}'")
+        if not isinstance(maxLogUnit,str):
+            raise TypeError(f"The unit of maximum number of file (maxLogUnit) must be a str, instead I've received a '{type(maxLog)}'")
+        if not maxLogUnit in self._SUPPORTED_LOG_UNIT:
+            raise ValueError(f"Unsupported option {maxLogUnit}, it should be in {self._SUPPORTED_LOG_UNIT}")
+        
+        # Check directory
+        absPath_dir = os.path.abspath(directory)
+        if not os.path.isdir(directory):
+            raise FileNotFoundError(f"The directory {absPath_dir} wasn't found!")
+
+        # Initialize instance:
+        # --------------------
+        super().__init__(level, filter)
+        self._directory = absPath_dir
+        self._filename_fmt = self._LOG_FILE_FMT.format(base=baseFilename,timeSep=timeSep,ext=log_ext)
+        self._filename_pattern = re.compile(self._ISO_FMT_STR_PATTERN.format(base=baseFilename,timeSep=timeSep,ext=log_ext))
+        self._current_file = None
+
+        self._tz = tz
+
+        self._maxSize = maxSizeFile
+        self._maxLog = maxLog
+        self._maxLogUnit = maxLogUnit
+
+
+    # Dunder methods:
+    # ---------------
+
+    def __len__(self) -> int:
+        return len(self.logfiles)
+
+
+    # Methods for creating/cleaning filelog:
+    # --------------------------------------
+
+    def _clean_num(self,tmp_rec=0):
+        """ Check all known logfile from number and remove the oldest one if their is too many. """
+        _logFiles = self.logfiles
+        if len(_logFiles) > self._maxLog:
+            oldest_file = None
+            oldest_time = datetime.datetime.now(self._tz)
+            for logfile in _logFiles:
+                if logfile.creationTime < oldest_time:
+                    oldest_file = logfile.name
+                    oldest_time = logfile.creationTime
+            
+            if not oldest_file:
+                raise Exception(f"The oldest logfile found is in the future!! The logfile name: {oldest_file}")
+            
+            file_to_remove = os.path.join(self._directory,oldest_file)
+            os.remove( os.path.abspath(file_to_remove) )
+
+        # Recursive calls until their is no more than self._maxLog files,
+        # That should never happen, but it's their just in case.
+        if len(self.logfiles) > self._maxLog:
+            warnings.warn("At one point, log's files weren't correctly cleaned (logFile by number) !!")
+            self._clean_num(tmp_rec=tmp_rec+1)
+
+    def _clean_date(self):
+        """ Check all known logfile from date """
+
+        # Compute max_timedelta
+        if self._maxLogUnit == 'day':
+            max_timedelta = datetime.timedelta(days=self._maxLog)
+        elif self._maxLogUnit == 'week':
+            max_timedelta = datetime.timedelta(weeks=self._maxLog)
+        elif self._maxLogUnit == 'month':
+            max_timedelta = datetime.timedelta(weeks=self._maxLog*4)
+        else:
+            raise ValueError(f"Unrecognized _maxLogUnit '{self._maxLogUnit}' when cleaning logfiles by date!")
+        
+        # Remove old logfiles
+        now = datetime.datetime.now(self._tz)
+        for file in self.logfiles:
+            dt = now - file.creationTime
+            if dt > max_timedelta:
+                os.remove( os.path.abspath(file.name) )
+            if dt < datetime.timedelta(0):
+                raise Exception(f"One logfile is in the future!! The logfile name: {file.name}")
+
+    def _request_new_logfile(self):
+        """ Create a new log file and then call right method for cleaning logfile """
+
+        # Create new file
+        now = datetime.datetime.now(self._tz)
+        filename_components = {
+            "date": now.date().isoformat(),
+            "time": now.timetz().isoformat()
+        }
+        new_filename = self._filename_fmt.format(**filename_components)
+        new_file = os.path.join(self._directory,new_filename)
+        try:
+            with open(new_file,'x') as f:
+                os.fsync(f.fileno())
+        except Exception as e:
+            raise Exception(f"Error while requesting a new logfile: {e}")
+        
+
+        # Clean logfiles
+        if self._maxLogUnit == "file":
+            self._clean_num()
+        else:
+            self._clean_date()
+
+        return new_file
+
+
+    # Private methods:
+    # ----------------
+
+    def _reconstruct_time_from_filename(self,filename:str):
+        if re_match := self._filename_pattern.match(filename):
+            
+            # Extract components
+            date = re_match.group('date')
+            time = re_match.group('time')
+            ms = re_match.group('time_ms')
+            tz = re_match.group('tz')
+
+            # Reconstruct datetime.datetime
+            reconstructed_datetime_str = f"{date}T{time}{'.'+str(ms) if ms else ''}{tz if tz else ''}"
+            return datetime.datetime.fromisoformat(reconstructed_datetime_str)
+        else:
+            raise ValueError(f"The format of filename '{filename}' is invalid!")
+
+    def _write(self, msg:LogMessage):
+        """ Write a message to the current logfile.
+
+        If their no current_file (ie for the first message to be logged) or if it's
+        size is greater than the max authorized, create a new file and clean-up logfiles
+        according to user will, then write the message.
+        """
+
+        if not self._current_file or os.path.getsize( self._current_file ) > self._maxSize:
+            self._current_file = self._request_new_logfile()
+
+        with open(self._current_file,'a') as f:
+            f.write( str(msg) )
+            os.fsync(f.fileno())
